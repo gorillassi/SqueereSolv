503084392c89e6526f98a97785bfbbee402bf831
Yasinchiiik <54998379+YasinAbuAssi@users.noreply.github.com>
2022-08-18 23:52:27 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cmath>
+
+int main()
+{
+    int a, b, c, ans;
+    float D;
+    scanf("%d %d %d", &a, &b, &c);
+
+    D = b*b - 4*a*c;
+
+    if (D > 0.0)
+    {
+        printf("2\n");
+    }
+    else if (D == 0.0)
+    {
+        printf("1\n");
+    }
+    else
+    {
+        printf("0\n");
+    }
+
+}
