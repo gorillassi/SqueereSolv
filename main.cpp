56efa04d8e156f72d57a1c8c863e6872a57d8676
Yasinchiiik <54998379+YasinAbuAssi@users.noreply.github.com>
2022-08-26 00:17:15 +0300

    25Finally

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <math.h>
 #include <stdbool.h>
 #include <assert.h>
 #include "solver.h"
-#include "output.h"
+#include "ioutput.h"
 #include "common.h"
+#include "test.h"
 
 
 int main(){ 
-    printf("Program: solver equation\n"
-           "Enter your numbers:\n");
-
+    greeting();
     double a = 0.0, b = 0.0, c = 0.0, x1 = 0.0, x2 = 0.0;
     int n = 0, NumOfRoots  = 0;
-    
-    while((scanf("%lf %lf %lf", &a, &b, &c) != 3)){        
-        while(getchar() != '\n'){}
-        printf("Wrong input, please try again...\n");
-    }
+
+    intput(&a, &b, &c);
 
     solver(a, b, c, &x1, &x2, &NumOfRoots);
     output(NumOfRoots, x1, x2);
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+#include "solver.h"
+#include "ioutput.h"
+#include "common.h"
+
+void test(){
+    
+}
